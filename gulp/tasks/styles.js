1c5afe4ae82f2813526662e30f617f5f437e75c1
chasevida <mark@chasevida.com>
2015-02-23 15:43:45 +1300

    fix(styles): fix css autoprefixer

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,6 +1,6 @@
 // gulp/tasks/styles.js
 
-var autoprefixer = require('autoprefixer-stylus'),
+var autoprefixer = require('autoprefixer-core'),
     axis         = require('axis-css'),
     config       = require('../config'),
     gulp         = require('gulp'),
@@ -8,6 +8,7 @@ var autoprefixer = require('autoprefixer-stylus'),
     errors       = require('../util/error-handler'),
     livereload   = require('gulp-livereload'),
     nib          = require('nib'),
+    postcss      = require('gulp-postcss'),
     rupture      = require('rupture'),
     stylus       = require('gulp-stylus');
 
@@ -18,11 +19,16 @@ gulp.task('styles', function () {
 
     var stylusOptions = {
         compress: !config.env.development ? true : false,
-        use: [nib(), rupture(), axis(), autoprefixer()]
+        use: [nib(), rupture(), axis()]
     };
+
+    var processors = [
+        autoprefixer({browsers: ['last 2 version']})
+    ];
 
     return gulp.src(config.paths.src + 'styles/style.styl')
         .pipe(stylus(stylusOptions))
+        .pipe(postcss(processors))
         .on('error', errors)
         .pipe(gulp.dest( config.paths.dist + 'css/'))
         .pipe(gulpif(config.env.development, livereload()));
