7c49812c0119c9f07eb0cb4bbcc57bd22b1227bc
chasevida <mark@chasevida.com>
2015-03-27 16:56:51 +1300

    js(Sourcemaps): add js sourcemap support

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -5,13 +5,12 @@ var browserify  = require('browserify'),
     concat      = require('gulp-concat'),
     errors      = require('../util/error-handler'),
     gulp        = require('gulp'),
-    gulpif      = require('gulp-if'),
     logger      = require('../util/bundle-logger'),
+    minifyify   = require('minifyify'),
     reload      = require('browser-sync').reload,
-    source      = require('vinyl-source-stream'),
     streamify   = require('gulp-streamify'),
     stringify   = require('stringify'),
-    uglify      = require('gulp-uglify');
+    vinylSource = require('vinyl-source-stream');
 
 
 // Concatenate libs
@@ -20,45 +19,43 @@ gulp.task('scripts:libs', function() {
 
     gulp.src([
             './client/vendor/lodash/dist/lodash.min.js',
-            './client/vendor/angular/angular.js',
+            './client/vendor/angular/angular.min.js',
             './client/vendor/angular-animate/angular-animate.js',
             './client/vendor/angular-ui-router/release/angular-ui-router.js',
             './client/vendor/angulartics/dist/angulartics.min.js',
             './client/vendor/angulartics/dist/angulartics-ga.min.js',
             './client/vendor/fastclick/lib/fastclick.js'
         ])
-        .pipe(concat('libs.js'))
+        .pipe(concat('libs.min.js'))
         .pipe(gulp.dest(config.paths.dist + 'js'));
 
 });
+
 
 // Browserify
 
 gulp.task('scripts', ['templates'], function() {
 
-    var uglifyOptions = {
-        mangle: false,
-        compress: {
-            pure_funcs: ['console.log']
-        }
-    };
-
     var bundleStream = browserify({
-        entries: [config.paths.src + 'js/main'],
-        extensions: ['.js', '.html']
+        entries:    [config.paths.src + 'js/main'],
+        extensions: ['.js', '.html'],
+        debug:      true,
     }).transform(stringify(['.html']));
 
+    bundleStream.plugin('minifyify', {
+        output: config.paths.dist + 'js/app.min.map.json',
+        map:    'app.min.map.json'
+    });
 
     var bundle = function() {
         logger.start();
 
         return bundleStream
-            .bundle({ debug: true })
+            .bundle()
             .on('error', errors)
-            .pipe(source('main.js'))
-            .pipe(gulpif(!config.env.development, streamify(uglify(uglifyOptions))))
+            .pipe(vinylSource('app.min.js'))
             .pipe(gulp.dest(config.paths.dist + 'js'))
-            .pipe(gulpif(config.env.development, reload({stream: true})))
+            .pipe(reload({stream: true}))
             .on('update', bundle)
             .on('end', logger.end);
     };
